1cb23e45cd453925965f4ccad2940d5dbbce8e3c
M4Samoylov <m4samoylov@yandex.ru>
2024-02-16 09:47:54 +0300

    bugfix-branch: new text

--- ltssons_2/src/main/java/org/example/Main.java
+++ ltssons_2/src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! Hello world! Hello world!");
-        System.out.println("Текст новой фичи");
+        System.out.println("Текст новой фичи bugfix-branch");
     }
 }
