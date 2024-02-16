2452854cf8b9ef9971f4c6b0fef6cd1b05284e07
M4Samoylov <m4samoylov@yandex.ru>
2024-02-16 09:43:14 +0300

    feature: new text

--- ltssons_2/src/main/java/org/example/Main.java
+++ ltssons_2/src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! Hello world! Hello world!");
+        System.out.println("Текст новой фичи");
     }
 }
